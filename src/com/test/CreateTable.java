129db097fb88407db6495aa8c1a57dd82db95ca9
DC <1282324479@qq.com>
2017-12-27 23:32:10 +0800

    利用MySQL实现ATM机系统

--- src/com/test/CreateTable.java
+++ src/com/test/CreateTable.java
@@ -14,15 +14,16 @@ public class CreateTable {
     public static void main(String[] args) {
         Connection conn = DBUtils.getConnection();
         String sql = "CREATE TABLE IF NOT EXISTS `user` (" +
-                "`ID_num` VARCHAR (255) NOT NULL, " +
-                "`name` VARCHAR ( 255 ) NOT NULL," +
-                "`sex` VARCHAR ( 255 ) NOT NULL," +
-                "`age` VARCHAR ( 255 ) NOT NULL," +
-                "`balance` VARCHAR ( 255 ) NOT NULL," +
-                "`password` VARCHAR ( 255 ) NOT NULL," +
+                "`ID_num` VARCHAR (255) DEFAULT NULL, " +
+                "`name` VARCHAR ( 255 ) DEFAULT NULL," +
+                "`sex` VARCHAR ( 255 ) DEFAULT NULL," +
+                "`age` VARCHAR ( 255 ) DEFAULT NULL," +
+                "`balance` VARCHAR ( 255 ) DEFAULT NULL," +
+                "`password` VARCHAR ( 255 ) DEFAULT NULL," +
                 "`id` BIGINT ( 18 ) UNSIGNED NOT NULL AUTO_INCREMENT," +
-                "`registered_city` VARCHAR ( 255 ) NOT NULL," +
-                "`phone_num` VARCHAR ( 255 ) NOT NULL," +
+                "`registered_city` VARCHAR ( 255 ) DEFAULT NULL," +
+                "`adress` VARCHAR ( 255 ) DEFAULT NULL,"+
+                "`phone_num` VARCHAR ( 255 ) DEFAULT NULL," +
                 "PRIMARY KEY ( `id` ) " +
                 ") ENGINE = INNODB AUTO_INCREMENT = 10000000000000000 DEFAULT CHARSET = utf8;";
         Statement stat = null;
